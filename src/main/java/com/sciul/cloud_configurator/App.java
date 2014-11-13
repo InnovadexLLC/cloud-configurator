b2c31338184c5c10bd71447f76641ee6ae732369
sumeet rohatgi <sum.rohatgi@gmail.com>
2014-11-13 14:32:21 -0800

    ensuring AWS API key is specified as an ENV var.

--- src/main/java/com/sciul/cloud_configurator/App.java
+++ src/main/java/com/sciul/cloud_configurator/App.java
@@ -5,6 +5,9 @@ import com.amazonaws.services.cloudformation.model.ListStacksResult;
 
 public class App {
 
+  private static final String ACCESS_KEY = "AWS_ACCESS_KEY";
+  private static final String SECRET_KEY = "AWS_SECRET_KEY";
+
   public void App() {
 
   }
@@ -16,6 +19,10 @@ public class App {
   }
 
   public static void main(String[] args) {
+    if (!System.getenv().containsKey(ACCESS_KEY) || !System.getenv().containsKey(SECRET_KEY)) {
+      System.err.print("Please define aws api keys as env vars ("+ACCESS_KEY+" & "+SECRET_KEY+")!!");
+      System.exit(1);
+    }
     App app = new App();
     app.run();
   }
