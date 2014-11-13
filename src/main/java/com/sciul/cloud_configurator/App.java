49a38f1bd88f1ed32c2a5e76e6c0d461502b6730
sumeet rohatgi <sum.rohatgi@gmail.com>
2014-11-13 14:22:48 -0800

    started calling amazon cloud formation

--- src/main/java/com/sciul/cloud_configurator/App.java
+++ src/main/java/com/sciul/cloud_configurator/App.java
@@ -1,8 +1,22 @@
 package com.sciul.cloud_configurator;
 
+import com.amazonaws.services.cloudformation.AmazonCloudFormationClient;
+import com.amazonaws.services.cloudformation.model.ListStacksResult;
+
 public class App {
 
+  public void App() {
+
+  }
+
+  public void run() {
+    AmazonCloudFormationClient clt = new AmazonCloudFormationClient();
+    ListStacksResult lst = clt.listStacks();
+    System.out.println("lst: " + lst);
+  }
+
   public static void main(String[] args) {
-    System.out.println("hello world");
+    App app = new App();
+    app.run();
   }
 }
