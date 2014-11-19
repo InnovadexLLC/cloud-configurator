4d12b1cb6430340ccd6d35e56ba933d0f6e1e417
sumeet rohatgi <sum.rohatgi@gmail.com>
2014-11-18 22:31:22 -0800

    added antlr4, refactored spring context

--- src/main/java/com/sciul/cloud_configurator/App.java
+++ src/main/java/com/sciul/cloud_configurator/App.java
@@ -2,15 +2,13 @@ package com.sciul.cloud_configurator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan
-@PropertySource("cloud-configure-app.properties")
+@PropertySource("classpath:cloud-configure-app.properties")
 public class App {
 
   private static Logger logger = LoggerFactory.getLogger(App.class);
@@ -18,7 +16,7 @@ public class App {
   public static void main(String[] args) {
     try {
 
-      ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+      ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
       CloudConfiguratorApp obj = (CloudConfiguratorApp) context.getBean("configure");
 
       obj.configure(args);
@@ -28,4 +26,10 @@ public class App {
       System.exit(1);
     }
   }
+
+  @Bean(name = "configure")
+  public CloudConfiguratorApp configureCloud() {
+    return new CloudConfiguratorApp();
+  }
+
 }
