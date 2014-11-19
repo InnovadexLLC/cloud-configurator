275a20eea63a3573f78cfce1c6d5366e5a0198d4
sumeet rohatgi <sum.rohatgi@gmail.com>
2014-11-18 22:38:42 -0800

    removed more cruft

--- src/main/java/com/sciul/cloud_configurator/App.java
+++ src/main/java/com/sciul/cloud_configurator/App.java
@@ -1,10 +1,13 @@
 package com.sciul.cloud_configurator;
 
+import com.amazonaws.services.cloudformation.model.DescribeStacksResult;
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan
@@ -13,11 +16,15 @@ public class App {
 
   private static Logger logger = LoggerFactory.getLogger(App.class);
 
+  private CommandLineParser parser = new BasicParser();
+  @Autowired
+  AwsProvider provider;
+
   public static void main(String[] args) {
     try {
 
       ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
-      CloudConfiguratorApp obj = (CloudConfiguratorApp) context.getBean("configure");
+      App obj = (App) context.getBean("configure");
 
       obj.configure(args);
 
@@ -28,8 +35,60 @@ public class App {
   }
 
   @Bean(name = "configure")
-  public CloudConfiguratorApp configureCloud() {
-    return new CloudConfiguratorApp();
+  public App myApp() {
+    return new App();
+  }
+
+  public void configure(String... args) {
+    try {
+
+      Options options = new Options() {
+        {
+          addOption("r", "region", true, "aws region, example: us-west-2");
+          addOption("e", "environment", true, "engineering environment, example: prod, dev, qa");
+          addOption("c", "command", true, "command to run, example: update, list");
+          addOption("h", "help", false, "this help message");
+        }
+      };
+
+      final CommandLine cmd = parser.parse(options, args);
+
+      if (cmd.hasOption("h")) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(80, "run.sh -r <region> -c <command> [-e <environment>] [-h]",
+            "Configure your cloud environments", options, "");
+        System.exit(0);
+      }
+
+      if (!cmd.hasOption("c")) {
+        throw new RuntimeException("no command specified");
+      }
+
+      provider.setRegion(cmd.getOptionValue("r"));
+
+      switch (cmd.getOptionValue("c")) {
+        case "update":
+          logger.debug("update environment: {} for region: {}", cmd.getOptionValue("e"), cmd.getOptionValue("r"));
+          provider.createStack(cmd.getOptionValue("e"));
+          logger.debug("****************Stack Creation Started****************");
+          break;
+        case "list":
+          logger.debug("list environments for region: {}", cmd.getOptionValue("r"));
+          DescribeStacksResult dst = null;
+          if (cmd.getOptionValue("e") != null && !cmd.getOptionValue("e").equalsIgnoreCase("")) {
+            dst = provider.describeStacks(cmd.getOptionValue("e"));
+          } else {
+            dst = provider.describeStacks(null);
+          }
+          logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+          logger.debug("dst: {}", dst);
+          logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+          break;
+      }
+
+    } catch (ParseException e) {
+      throw new RuntimeException("Unable to parse command line options", e);
+    }
   }
 
 }
