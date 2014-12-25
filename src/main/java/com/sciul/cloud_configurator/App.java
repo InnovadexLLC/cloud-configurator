404bde459de7c19888913a92732fdadd7cf85e17
sumeet rohatgi <sum.rohatgi@gmail.com>
2014-12-25 12:46:51 -0800

    using provider interface

--- src/main/java/com/sciul/cloud_configurator/App.java
+++ src/main/java/com/sciul/cloud_configurator/App.java
@@ -20,7 +20,7 @@ public class App {
   private CommandLineParser parser = new BasicParser();
 
   @Autowired
-  AwsProvider provider;
+  Provider provider;
 
   public static void main(String[] args) {
     try {
