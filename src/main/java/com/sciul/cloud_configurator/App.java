674d1260e17404914f910da1a83690e62d18c393
sumeet rohatgi <sum.rohatgi@gmail.com>
2015-01-02 10:00:09 -0800

    refactored to be more sane...

--- src/main/java/com/sciul/cloud_configurator/App.java
+++ src/main/java/com/sciul/cloud_configurator/App.java
@@ -42,108 +42,133 @@ public class App {
   }
 
   public void configure(String... args) {
-    try {
+    Options options = new Options() {
+      {
+        addOption("c", "command", true, "command to run, example: update, list");
+        addOption("r", "region", true, "aws region");
+        addOption("a", "api domain", true, "api hosted on this domain");
+        addOption("w", "web domain", true, "webapp hosted on this domain");
+        addOption("p", "prefix", true, "environment prefix");
+        addOption("f", "file", true, "file to read input/ write output");
+        addOption("h", "help", false, "this help message");
+      }
+    };
 
-      Options options = new Options() {
-        {
-          addOption("c", "command", true, "command to run, example: update, list");
-          addOption("r", "region", true, "aws region");
-          addOption("a", "api domain", true, "api hosted on this domain");
-          addOption("w", "web domain", true, "webapp hosted on this domain");
-          addOption("p", "prefix", true, "environment prefix");
-          addOption("f", "file", true, "file to read input/ write output");
-          addOption("h", "help", false, "this help message");
-        }
-      };
+    try {
 
       final CommandLine cmd = parser.parse(options, args);
 
       if (cmd.hasOption("h")) {
-        HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp(80, "run.sh -c <update|list|generate> -r <region> -p <prefix> -w <web domain> -a <api domain> [-f <filename>] [-h]",
-            "Configure your cloud environment", options, "");
+        writeHelp(options);
         return;
       }
 
       if (!cmd.hasOption("c")) {
-        throw new RuntimeException("no command specified");
+        throw new MissingArgumentException("no command specified");
       }
 
       if (!cmd.hasOption("r")) {
-        throw new RuntimeException("no region specified");
-      }
-
-      if (!cmd.hasOption("p")) {
-        throw new RuntimeException("no prefix specified");
-      }
-
-      if (!cmd.hasOption("a")) {
-        throw new RuntimeException("no api domain specified");
-      }
-
-      if (!cmd.hasOption("w")) {
-        throw new RuntimeException("no web domain specified");
+        throw new MissingArgumentException("no region specified");
       }
 
       String region = cmd.getOptionValue("r");
-      String prefix = cmd.getOptionValue("p");
-      String webDomain = cmd.getOptionValue("w");
-      String apiDomain = cmd.getOptionValue("a");
-
-      //Template template = new Template(prefix, region, apiDomain, webDomain);
-
-      Template template = new Template(prefix, region, webDomain, "", apiDomain, "", "C*", "MQ", "ES");
-
       provider.setRegion(region);
 
       switch (cmd.getOptionValue("c")) {
         case "generate":
-          String templateBody = provider.generateStackTemplate(template);
-          if (!cmd.hasOption("f")) {
-            logger.debug("generated template: {}", template);
-            break;
-          }
-
-          Writer writer = null;
-
-          try {
-            writer = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(cmd.getOptionValue("f")), "utf-8"));
-            writer.write(templateBody);
-          } catch (IOException ex) {
-            throw new RuntimeException("unable to write to file");
-          } finally {
-            try {
-              writer.close();
-              logger.info("wrote template to file: {}", cmd.getOptionValue("f"));
-            } catch (Exception ex) {
-              throw new RuntimeException("unable to write to file");
-            }
-          }
+          generateApplicationConfiguration(cmd, region);
           break;
 
         case "update":
-          provider.createStack(template);
-
-          logger.debug("****************Stack Creation Started****************");
+          updateApplication(cmd, region);
           break;
 
         case "list":
-          logger.debug("list environments for region: {}", cmd.getOptionValue("r"));
-          DescribeStacksResult dst = null;
-          if (cmd.getOptionValue("e") != null && !cmd.getOptionValue("e").equalsIgnoreCase("")) {
-            dst = provider.describeStacks(cmd.getOptionValue("e"));
-          } else {
-            dst = provider.describeStacks(null);
-          }
-          logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-          logger.debug("dst: {}", dst);
-          logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+          listExistingApplications(cmd, region);
           break;
       }
 
     } catch (ParseException e) {
+      writeHelp(options);
       throw new RuntimeException("Unable to parse command line options", e);
+    }
+  }
+
+  private void writeHelp(Options options) {
+    HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp(80, "run.sh -c <update|list|generate> -r <region> -p <prefix> -w <web domain> " +
+            "-a <api domain> [-f <filename>] [-h]",
+        "Configure your cloud environment", options, "");
+  }
+
+  private Template buildTemplate(CommandLine cmd, String region) throws MissingArgumentException {
+    if (!cmd.hasOption("p")) {
+      throw new MissingArgumentException("no prefix specified");
+    }
+
+    if (!cmd.hasOption("a")) {
+      throw new MissingArgumentException("no api domain specified");
+    }
+
+    if (!cmd.hasOption("w")) {
+      throw new MissingArgumentException("no web domain specified");
+    }
+
+    String prefix = cmd.getOptionValue("p");
+    String webDomain = cmd.getOptionValue("w");
+    String apiDomain = cmd.getOptionValue("a");
+
+    //Template template = new Template(prefix, region, apiDomain, webDomain);
+
+    return new Template(prefix, region, webDomain, "", apiDomain, "", "C*", "MQ", "ES");
+  }
+
+  private void updateApplication(CommandLine cmd, String region) throws MissingArgumentException {
+    Template template = buildTemplate(cmd, region);
+
+    provider.createStack(template);
+
+    logger.debug("****************Stack Creation Started****************");
+  }
+
+  private void listExistingApplications(CommandLine cmd, String region) {
+    logger.debug("list environments for region: {}", region);
+
+    DescribeStacksResult dst = null;
+    if (cmd.hasOption("e")) {
+      dst = provider.describeStacks(cmd.getOptionValue("e"));
+    } else {
+      dst = provider.describeStacks(null);
+    }
+
+    logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    logger.debug("dst: {}", dst);
+    logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+  }
+
+  private void generateApplicationConfiguration(CommandLine cmd, String region) throws MissingArgumentException {
+    Template template = buildTemplate(cmd, region);
+    String templateBody = provider.generateStackTemplate(template);
+    if (!cmd.hasOption("f")) {
+      logger.debug("generated template: {}", template);
+      return;
+    }
+
+    Writer writer = null;
+
+    try {
+      writer = new BufferedWriter(new OutputStreamWriter(
+          new FileOutputStream(cmd.getOptionValue("f")), "utf-8"));
+      writer.write(templateBody);
+    } catch (IOException ex) {
+      throw new RuntimeException("unable to write to file", ex);
+    } finally {
+      try {
+        writer.close();
+        logger.info("wrote template to file: {}", cmd.getOptionValue("f"));
+      } catch (Exception ex) {
+        throw new RuntimeException("unable to write to file", ex);
+      }
     }
   }
 
