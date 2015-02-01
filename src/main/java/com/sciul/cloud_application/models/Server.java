c23de17d2f0f7551f8921933b945fc84ce3b4d59
sumeet rohatgi <sum.rohatgi@gmail.com>
2015-01-31 18:23:33 -0800

    new model class for server

---
+++ src/main/java/com/sciul/cloud_application/models/Server.java
@@ -0,0 +1,16 @@
+package com.sciul.cloud_application.models;
+
+/**
+ * Created by sumeetrohatgi on 1/31/15.
+ */
+public class Server {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
